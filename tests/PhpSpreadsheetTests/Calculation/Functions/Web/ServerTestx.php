0b136d1e6e7129b0d5245d4395cf68bfd96032aa
oleibman <10341515+oleibman@users.noreply.github.com>
2026-07-12 00:07:04 -0700

    Blip

--- tests/PhpSpreadsheetTests/Calculation/Functions/Web/ServerTestx.php
+++ tests/PhpSpreadsheetTests/Calculation/Functions/Web/ServerTestx.php
@@ -7,7 +7,7 @@ use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Process\Process;
 
-class ServerTest extends TestCase
+class ServerTestx extends TestCase
 {
     private static Process $httpServer;
 
@@ -20,7 +20,6 @@ class ServerTest extends TestCase
     public static function setUpBeforeClass(): void
     {
         $commandLine = 'php -S ' . self::SERVER . ' -t ' . __DIR__;
-        var_dump($commandLine);
 
         self::$httpServer = Process::fromShellCommandline($commandLine);
         self::$httpServer->start();
